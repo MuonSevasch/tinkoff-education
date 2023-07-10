b70f8696861e66eea558fb1f543a131ba9c56a12
ar.zaytsev <ar.zaytsev@tinkoff.ru>
2023-07-10 11:00:38 +0300

    project: 1,2 tasks

---
+++ src/main/java/com/example/education/Client.java
@@ -0,0 +1,14 @@
+package com.example.education;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Client {
+    private int id;
+    private String fullName;
+    private String city;
+
+
+}
